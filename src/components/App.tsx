942a7ae5b5640730eee07c90aff8c8c9d5325190
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-09-23 12:45:49 +0200

    working on form

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,5 @@
-export default function App() {
+import Step1 from "./Step1";
 
+export default function App() {
+  return <Step1 />;
 }
--- src/components/Step1.tsx
+++ src/components/Step1.tsx
@@ -1,75 +1,94 @@
-import useForm from "../hooks/useForm";
+import { useState } from "react";
 
-export default function Step1() { 
-  const {values, handleChange, handleSubmit } = useForm();
-  console.log("Step 1");
+const defaultFormFields = {
+  name: "",
+  email: "",
+  phone: "",
+};
+
+export default function Step1() {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { name, email, phone } = formFields;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+    console.log(formFields);
+  };
+
   return (
-    <form data-step-content className='flex flex-col justify-evenly m-5'>
-      <h1 className='text-5xl pt-10 text-marine-blue font-bold'>
+    <form data-step-content className="flex flex-col justify-evenly m-5">
+      <h1 className="text-5xl pt-10 text-marine-blue font-bold">
         Personal info
       </h1>
-      <p className='text-cool-gray'>
+      <p className="text-cool-gray">
         Please provide your name, email address, and phone number.
       </p>
 
-      <div className='flex flex-col'>
+      <div className="flex flex-col">
         <label
-          className='text-marine-blue pb-2 inline-flex justify-between'
-          htmlFor='name'
+          className="text-marine-blue pb-2 inline-flex justify-between"
+          htmlFor="name"
         >
           <span>Name</span>
-          <span className='text-red-600' data-input-error='email'></span>
+          <span className="text-red-600" data-input-error="email"></span>
         </label>
         <input
-          className='border p-2.5 rounded-md outline-none focus:border-purplish-blue'
-          type='text'
-          placeholder='e.g. Stephen King'
-          name='name'
-          id='name'
+          className="border p-2.5 rounded-md outline-none focus:border-purplish-blue"
+          type="text"
+          placeholder="e.g. Stephen King"
+          name="name"
+          onChange={handleChange}
+          value={name}
+          id="name"
           required
         />
       </div>
 
-      <div className='flex flex-col'>
+      <div className="flex flex-col">
         <label
-          className='text-marine-blue pb-2 inline-flex justify-between'
-          htmlFor='email'
+          className="text-marine-blue pb-2 inline-flex justify-between"
+          htmlFor="email"
         >
           <span>Email Address</span>
-          <span className='text-red-600' data-input-error='email'></span>
+          <span className="text-red-600" data-input-error="email"></span>
         </label>
         <input
-          className='border p-2.5 rounded-md outline-none focus:border-purplish-blue'
-          type='email'
-          placeholder='e.g. stephenking@lorem.com'
-          name='email'
-          id='email'
+          className="border p-2.5 rounded-md outline-none focus:border-purplish-blue"
+          type="email"
+          placeholder="e.g. stephenking@lorem.com"
+          name="email"
+          onChange={handleChange}
+          value={email}
+          id="email"
           required
         />
       </div>
 
-      <div className='flex flex-col'>
+      <div className="flex flex-col">
         <label
-          className='text-marine-blue pb-2 inline-flex justify-between'
-          htmlFor='phone'
+          className="text-marine-blue pb-2 inline-flex justify-between"
+          htmlFor="phone"
         >
           <span>Phone Number</span>
-          <span className='text-red-600' data-input-error='phone'></span>
+          <span className="text-red-600"></span>
         </label>
         <input
-          className='border p-2.5 rounded-md outline-none focus:border-purplish-blue'
-          type='text'
-          placeholder='e.g. +1 234 567 890'
-          name='phone'
-          id='phone'
+          className="border p-2.5 rounded-md outline-none focus:border-purplish-blue"
+          type="text"
+          placeholder="e.g. +1 234 567 890"
+          name="phone"
+          onChange={handleChange}
+          value={phone}
+          id="phone"
           required
         />
       </div>
 
       <button
-        type='button'
-        className='bg-marine-blue self-end text-white px-6 py-3 rounded-md font-bold'
-        data-step-next='step-1'
+        type="button"
+        className="bg-marine-blue self-end text-white px-6 py-3 rounded-md font-bold"
       >
         Next Step
       </button>
